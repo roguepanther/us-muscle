8ba2a5758bb35edbdd72cd20e09b19081adf43a2
roguepanther <roguepanthernoob@gmail.com>
2023-07-16 00:49:30 +0100

    Updated launcher activity to trigger on app launch

---
+++ app/src/main/java/com/nprkov/usmuscle/LauncherActivity.java
@@ -0,0 +1,31 @@
+package com.nprkov.usmuscle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
+public class LauncherActivity extends AppCompatActivity {
+
+    private static final long DELAY_DURATION = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                launchMainActivity();
+            }
+        }, DELAY_DURATION);
+    }
+
+    private void launchMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
